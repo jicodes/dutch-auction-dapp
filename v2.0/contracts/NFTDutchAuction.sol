cab27f370cc492f4bfc08992e5a6ee82b5e63497
JiCodes <liu.ji1@northeastern.edu>
2023-06-21 23:56:53 -0700

    update NFTDutchAuction contract

--- v2.0/contracts/NFTDutchAuction.sol
+++ v2.0/contracts/NFTDutchAuction.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract NFTDutchAuction {
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+
+contract NFTDutchAuction is ERC721Holder {
     address payable public seller;
+    IERC721 public nftContract;
+    uint256 public nftTokenId;
     uint256 public reservePrice;
     uint256 public numBlocksAuctionOpen;
     uint256 public offerPriceDecrement;
@@ -12,41 +17,36 @@ contract NFTDutchAuction {
     address public winner;
 
     constructor(
+        address erc721TokenAddress,
+        uint256 _nftTokenId,
         uint256 _reservePrice,
         uint256 _numBlocksAuctionOpen,
         uint256 _offerPriceDecrement
     ) {
         seller = payable(msg.sender);
+        nftContract = IERC721(erc721TokenAddress);
+        nftTokenId = _nftTokenId;
         reservePrice = _reservePrice;
         numBlocksAuctionOpen = _numBlocksAuctionOpen;
         offerPriceDecrement = _offerPriceDecrement;
-        initialPrice =
-            reservePrice +
-            numBlocksAuctionOpen *
-            offerPriceDecrement;
+        initialPrice = reservePrice + numBlocksAuctionOpen * offerPriceDecrement;
         startBlock = block.number;
         auctionEnded = false;
     }
 
-    function bid() public payable returns (address) {
-        
-        require(
-            block.number <= startBlock + numBlocksAuctionOpen,
-            "Auction is already ended"
-        );
+    function bid() public payable {
+        require(block.number <= startBlock + numBlocksAuctionOpen, "Auction is already ended");
         require(!auctionEnded, "Auction is already ended");
-        require(
-            msg.value >= currentPrice(),
-            "Bid must be higher than current price"
-        );
+        require(msg.value >= currentPrice(), "Bid must be higher than current price");
 
         auctionEnded = true;
-        seller.transfer(msg.value); // Transfer the funds to the owner immediately
+        seller.transfer(msg.value); // Transfer the funds to the seller immediately
+
+        // Transfer the NFT to the winner
+        nftContract.safeTransferFrom(address(this), msg.sender, nftTokenId);
+
         winner = msg.sender;
-
-        return winner;
     }
-
 
     function currentPrice() public view returns (uint256) {
         uint256 elapsedBlocks = block.number - startBlock;
